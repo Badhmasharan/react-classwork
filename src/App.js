6450cb3fab0ff189de7b638aaecd42723a75fda2
Badhmasharan <124858439+Badhmasharan@users.noreply.github.com>
2023-11-20 15:47:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Day2cw2 from './day2cw2';
+import ToggleMessage from './day3cw';
+import Day2pah from './day2pah';
+import Greet from './Day2cw1';
+import Transformation from './Day3pah';
+import Day4cw from './Day4cw';
+import Loginpg from './Day4pah';
+import List from './Day5cw';
+import MountingPhase from './Day5pah';
+
+
+function App() {
+  return (
+    <div className="App">
+    <MountingPhase/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Day4cw.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Day4cw1 from './Day4cw1'
+import Day4cw2 from './Day4cw2'
+import Day4cw3 from './Day4cw3'
+
+export default function Day4cw() {
+  return (
+    <div>
+        <Day4cw1/>
+        <br></br>
+        <Day4cw2/>
+        <br></br>
+        <Day4cw3/>
+    </div>
+  )
+}
